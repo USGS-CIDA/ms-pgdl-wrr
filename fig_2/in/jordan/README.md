5270cdb40649434cbdd3372e39b13c51bee71992
Alison Appling <aappling@usgs.gov>
2019-01-30 10:57:31 -0500

    more detail on jordan's data

--- fig_2/in/jordan/README.md
+++ fig_2/in/jordan/README.md
@@ -4,11 +4,11 @@ Data files are stored in https://drive.google.com/drive/u/0/folders/1MjNrYJ8k4WK
 
 ## Example set of files for a single model run (pretraining + training):
 
-* mendota_geometry.csv
-* mendota_meteo.feather
-* mendota_season_test.feather
-* mendota_season_training_500_profiles_experiment_01.feather
-* Mendota_temperatures.feather
+* mendota_geometry.csv - lake shape; cols for depths, areas
+* mendota_meteo.feather - daily values for ShortWave, LongWave, AirTemp, RelHum, WindSpeed, Rain, Snow
+* Mendota_temperatures.feather - GLM-predicted temperatures, for pre-training, in wide format (1 row per date, 1 col per depth)
+* mendota_season_training_500_profiles_experiment_01.feather - fine-tuning observations, cols for DateTime, Depth, temp
+* mendota_season_test.feather - test observations, cols for DateTime, Depth, temp
 
 # Other files in this folder
 
--- fig_2/in/jordan/explore_jordan_data.R
+++ fig_2/in/jordan/explore_jordan_data.R
@@ -7,9 +7,21 @@ library(tidyverse)
 # read data that Jordan 
 geometry <- readr::read_csv('fig_2/in/jordan/mendota_geometry.csv')
 meteo <- feather::read_feather('fig_2/in/jordan/mendota_meteo.feather')
-temperatures <- feather::read_feather('fig_2/in/jordan/Mendota_temperatures.feather') # GLM-predicted temperatures
+temperatures <- feather::read_feather('fig_2/in/jordan/Mendota_temperatures.feather')
 training <- feather::read_feather('fig_2/in/jordan/mendota_season_training_500_profiles_experiment_01.feather')
 test <- feather::read_feather('fig_2/in/jordan/mendota_season_test.feather')
 
-head(temperatures) 
-head(training)
+dim(geometry) # 30 2
+head(geometry) # cols: depths, areas
+
+dim(meteo) # 3186 8
+head(meteo) # cols: time, ShortWave, LongWave, AirTemp, RelHum, WindSpeed, Rain, Snow
+
+dim(temperatures) # 3185 52
+head(temperatures) # cols: DateTime, temp_0, temp_0.5, temp_1, temp_1.5, ..., temp_24.5, ice
+
+dim(training) # 11587 3
+head(training) # cols: DateTime, Depth, temp
+
+dim(test) # 15833 3
+head(test) # cols: DateTime, Depth, temp
