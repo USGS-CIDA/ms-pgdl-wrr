309a599b826ef64e39d7e9f14ae59fad24ed9bc9
Jordan S Read <jread@usgs.gov>
2019-05-20 07:30:57 -0500

    update w/ Jared's new results

--- fig_3/src/generate_figure_3.R
+++ fig_3/src/generate_figure_3.R
@@ -4,8 +4,8 @@ plot_calibrated_figure_3 <- function(){
 
   library(dplyr)
 
-  jared_pgdl <- readr::read_csv('fig_3/in/glm_uncal_vs_PGDL_rmses.csv')  %>%
-    mutate(PGDL = as.numeric(`PGDL rmse`)) %>% slice(1:68)
+  jared_pgdl <- readr::read_csv('~/Downloads/glm_uncal_vs_PGDL_rmses - glm_uncal_rmses.csv')  %>% #'fig_3/in/glm_uncal_vs_PGDL_rmses.csv'
+    mutate(PGDL = as.numeric(`PGDL - partial PT`), rnn = `DL - no PT`) %>% slice(1:68)
 
 
   glm_rmse <- readr::read_tsv('fig_3/in/glm_uncal_rmses.tsv') %>%
@@ -20,7 +20,7 @@ plot_calibrated_figure_3 <- function(){
   cal_GLM$nhd_id = sapply(1:nrow(cal_GLM), FUN = function(i) strsplit(cal_GLM$nhd_id[i], '[_]')[[1]][2])
 
   plot_data <- left_join(plot_data, cal_GLM, by = 'nhd_id') %>%
-    filter(!is.na(calibrated)) %>% select(nhd_id, GLM, PGDL, calibrated)
+    filter(!is.na(calibrated), !is.na(rnn)) %>% select(nhd_id, GLM, PGDL, rnn, calibrated)
 
   n_sims <- nrow(plot_data)
 
@@ -34,8 +34,7 @@ plot_calibrated_figure_3 <- function(){
   plot(NA, NA, xlim = xlim, ylim = ylim,
        ylab = 'Test RMSE (°C)', axes = FALSE, xaxs = 'i', yaxs = 'i', cex.lab = 1.5)
   library(beanplot)
-  # TEMPORARY UNTIL MODELS ARE RUN!!
-  plot_data$rnn <- plot_data$calibrated + rnorm(n = n_sims, sd = 0.1, mean = -0.2)
+
   plot_data <- select(plot_data, GLM, calibrated, rnn, PGDL)
 
   beanplot(plot_data$GLM, plot_data$calibrated, plot_data$rnn, plot_data$PGDL, maxwidth = 0.95, what=c(0,1,0,0), log = "", add = TRUE,
@@ -49,20 +48,6 @@ plot_calibrated_figure_3 <- function(){
     segments(x0 = x_bin-med_w/2, x1 = x_bin+med_w/2, y0 = median(plot_data[[mod_name]]), col = 'white', lwd = 2)
   }
 
-  diffs <- plot_data$GLM - plot_data$PGDL
-  highlight_num <- 5
-
-  highlight_max <- which(diffs >= (sort(diffs) %>% tail(highlight_num) %>% head(1)))
-  highlight_min <- which(diffs <= (sort(diffs, decreasing = TRUE) %>% tail(highlight_num) %>% head(1)))
-
-  for (x_bin in 1:(ncol(plot_data) - 1)){
-    from_mod_name <- names(plot_data)[x_bin]
-    to_mod_name <- names(plot_data)[x_bin + 1]
-    segments(x0 = x_bin+ind_w/2, x1 = x_bin + 1 - ind_w/2, y0 = plot_data[[from_mod_name]][highlight_min], y1 = plot_data[[to_mod_name]][highlight_min], col = 'red')
-    segments(x0 = x_bin+ind_w/2, x1 = x_bin + 1 - ind_w/2, y0 = plot_data[[from_mod_name]][highlight_max], y1 = plot_data[[to_mod_name]][highlight_max], col = 'green')
-
-  }
-
 
   axis(2, at = seq(0,10), las = 1, tck = -0.01, cex.axis = 1.5, lwd = 1.5)
   par(mai = c(0.2,0,0,0), mgp = c(2, 0.8,0))
@@ -70,6 +55,5 @@ plot_calibrated_figure_3 <- function(){
   par(mai = c(0.2,0,0,0), mgp = c(2,2.3,0))
   axis(1, at = c(-100, 1, 2, 3, 4, 1e10), labels = c("", expression("Based"['uncal']), expression("Based"['calibrated']), "Learning","Deep Learning", ""), tck = NA, cex.axis = 1.5, lwd = NA)
 
-  text(3, 1.8, labels = '?', col = 'white', pos = 1, cex = 6)
   dev.off()
 }
