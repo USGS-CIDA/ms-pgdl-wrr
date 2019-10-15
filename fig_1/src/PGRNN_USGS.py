9d809a2d7f1cff1d78efddb9ad9a3930c380927c
Alison Appling <aappling@usgs.gov>
2019-10-15 13:25:07 -0400

    remove unneeded comments, fig 1

--- fig_1/src/PGRNN_USGS.py
+++ fig_1/src/PGRNN_USGS.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 21 14:11:47 2018
-only LSTM_ + PHY input    PGRNN0
+Training for PGDL model
 @author: xiaoweijia
 """
 
@@ -19,14 +18,16 @@ parser.add_argument('--restore_path', default='fig_1/tmp/mendota/pretrain/model'
 parser.add_argument('--save_path', default='fig_1/tmp/mendota/train/model')
 parser.add_argument('--preds_path', default='fig_1/tmp/mendota/train/out')
 args = parser.parse_args()
-print(args)
 
+tf.reset_default_graph()
 random.seed(9001)
 
+
 ''' Declare constant hyperparameters '''
+
 learning_rate = 0.005
-epochs = 400
-state_size = 21
+epochs = 10#400
+state_size = 20
 input_size = 9
 phy_size = 10
 n_steps = 353
@@ -35,34 +36,24 @@ N_sec = 19
 elam = 0.005
 ec_threshold = 24
 
-''' Build Graph '''
-# Graph input/output
+''' Define Graph '''
 
-
-tf.reset_default_graph()
-
-x = tf.placeholder("float", [None, n_steps, input_size]) #tf.float32
-y = tf.placeholder("float", [None, n_steps]) #tf.int32
+x = tf.placeholder("float", [None, n_steps, input_size])
+y = tf.placeholder("float", [None, n_steps])
 m = tf.placeholder("float", [None, n_steps])
-bt_sz = tf.placeholder("int32", None) #tf.int32
-x_u = tf.placeholder("float", [None, n_steps, input_size]) 
-                  
-X=x
-lstm_cell = tf.contrib.rnn.BasicLSTMCell(state_size, forget_bias=1.0) 
+bt_sz = tf.placeholder("int32", None)
+x_u = tf.placeholder("float", [None, n_steps, input_size])
 
-state_series_x, current_state_x = tf.nn.dynamic_rnn(lstm_cell, X, dtype=tf.float32) #46*(500*7)
-w_fin = tf.get_variable('w_fin',[state_size, n_classes], tf.float32,
-                                 tf.random_normal_initializer(stddev=0.02))
-#b_p = tf.get_variable('b_p',[state_size],  tf.float32,
-#                                 initializer=tf.constant_initializer(0.0))
-b_fin = tf.get_variable('b_fin',[n_classes],  tf.float32,
-                                 initializer=tf.constant_initializer(0.0))
+lstm_cell = tf.contrib.rnn.BasicLSTMCell(state_size, forget_bias=1.0)
+
+state_series_x, current_state_x = tf.nn.dynamic_rnn(lstm_cell, x, dtype=tf.float32)
+w_fin = tf.get_variable('w_fin', [state_size, n_classes], tf.float32, tf.random_normal_initializer(stddev=0.02))
+b_fin = tf.get_variable('b_fin', [n_classes], tf.float32, initializer=tf.constant_initializer(0.0))
 
 pred=[]
-#pred_s = []
 for i in range(n_steps):
     tp1 = state_series_x[:,i,:]
-    pt = tf.matmul(tp1,w_fin)+b_fin 
+    pt = tf.matmul(tp1,w_fin)+b_fin
     pred.append(pt)
 
 pred = tf.stack(pred,axis=1)
@@ -72,102 +63,53 @@ m_s = tf.reshape(m,[-1,1])
 
 r_cost = tf.sqrt(tf.reduce_sum(tf.square(tf.multiply((pred_s-y_s),m_s)))/tf.reduce_sum(m_s))
 
-
+''' Define Physics Helper Functions '''
 
 def transformTempToDensity(temp):
-    # print(temp)
-    #converts temperature to density
-    #parameter:
-        #@temp: single value or array of temperatures to be transformed
     densities = 1000*(1-((temp+288.9414)*tf.pow(temp- 3.9863,2))/(508929.2*(temp+68.12963)))
-    # densities[:] = 1000*(1-((temp[:]+288.9414)*torch.pow(temp[:] - 3.9863))/(508929.2*(temp[:]+68.12963)))
-
     return densities
 
-def calculate_ec_loss(inputs, outputs, phys, depth_areas, n_depths, ec_threshold, combine_days=1):
-    #******************************************************
-    #description: calculates energy conservation loss
-    #parameters: 
-        #@inputs: features
-        #@outputs: labels
-        #@phys: features(not standardized) of sw_radiation, lw_radiation, etc
-        #@labels modeled temp (will not used in loss, only for test)
-        #@depth_areas: cross-sectional area of each depth
-        #@n_depths: number of depths
-        #@use_gpu: gpu flag
-        #@combine_days: how many days to look back to see if energy is conserved
-    #*********************************************************************************
-    n_sets = N_sec#= np.floor(inputs.size()[0] / n_depths)#sets of depths in batch
-    
-#    diff_vec = torch.empty((inputs.size()[1]))
-#    n_dates = inputs.size()[1]
-    # outputs = labels
-    
-#    outputs = outputs.view(outputs.size()[0], outputs.size()[1])
-    # print("modeled temps: ", outputs)
-    densities = transformTempToDensity(outputs)
-    # print("modeled densities: ", densities)
 
-    diff_per_set = [] #torch.empty(n_sets) 
-    for i in range(n_sets):
-        #loop through sets of n_depths
+def calculate_ec_loss(inputs, outputs, phys, depth_areas, n_depths, ec_threshold, combine_days=1):
+    densities = transformTempToDensity(outputs)
+
+    diff_per_set = []
+    #loop through sets of n_depths
+    for i in range(N_sec):
         #indices
         start_index = (i)*n_depths
         end_index = (i+1)*n_depths
 
         #calculate lake energy for each timestep
-        lake_energies = calculate_lake_energy(outputs[start_index:end_index,:], densities[start_index:end_index,:], depth_areas)
+        lake_energies = calculate_lake_energy(
+            outputs[start_index:end_index,:], densities[start_index:end_index,:], depth_areas)
+
         #calculate energy change in each timestep
         lake_energy_deltas = calculate_lake_energy_deltas(lake_energies, combine_days, depth_areas[0])
         lake_energy_deltas = lake_energy_deltas[1:]
-        #calculate sum of energy flux into or out of the lake at each timestep
-        # print("dates ", dates[0,1:6])
-        lake_energy_fluxes = calculate_energy_fluxes(phys[start_index,:,:], outputs[start_index,:], combine_days)
-#        ### can use this to plot energy delta and flux over time to see if they line up
-#        doy = np.array([datetime.datetime.combine(date.fromordinal(x), datetime.time.min).timetuple().tm_yday  for x in dates[start_index,:]])
-#        doy = doy[1:-1]
-        
-#        print(lake_energy_deltas)
-        diff_vec = tf.abs(lake_energy_deltas - lake_energy_fluxes) #.abs_()
-        
-        # mendota og ice guesstimate
-        # diff_vec = diff_vec[np.where((doy[:] > 134) & (doy[:] < 342))[0]]
 
-        #actual ice
-#        print(diff_vec)
-#        print(phys)
-        tmp_mask = 1-phys[start_index+1,1:-1,9] #(phys[start_index+1,:,9] == 0)
-#        print(tmp_mask)
-#        print(diff_vec)
-#        print(tmp_mask)
+        #calculate sum of energy flux into or out of the lake at each timestep
+        lake_energy_fluxes = calculate_energy_fluxes(phys[start_index,:,:], outputs[start_index,:], combine_days)
+        diff_vec = tf.abs(lake_energy_deltas - lake_energy_fluxes)
+
+        #ice mask
+        tmp_mask = 1-phys[start_index+1,1:-1,9]
         tmp_loss = tf.reduce_mean(diff_vec*tf.cast(tmp_mask,tf.float32))
         diff_per_set.append(tmp_loss)
-        
-##        print(phys)
-#        diff_vec = diff_vec[tf.where((phys[1:(n_depths-tf.shape(diff_vec)[0]-1),9] == 0))[0]]
-#        # #compute difference to be used as penalty
-#        if tf.shape(diff_vec)[0]==0: #.size() == torch.Size([0]):
-#            diff_per_set.append(0) #diff_per_set[i] = 0
-#        else:
-#            diff_per_set.append(tf.reduce_mean(diff_vec)) #diff_per_set[i] = diff_vec.mean()
 
     diff_per_set_r = tf.stack(diff_per_set)
-    
+
     diff_per_set = tf.clip_by_value(diff_per_set_r - ec_threshold, clip_value_min=0,clip_value_max=999999)
-#    diff_per_set = torch.clamp(diff_per_set - ec_threshold, min=0)
-    return tf.reduce_mean(diff_per_set),diff_vec,diff_per_set_r,diff_per_set #, lake_energy_deltas, lake_energy_fluxes#.mean()
+
+    return tf.reduce_mean(diff_per_set),diff_vec,diff_per_set_r,diff_per_set
+
 
 def calculate_lake_energy(temps, densities, depth_areas):
     #calculate the total energy of the lake for every timestep
     #sum over all layers the (depth cross-sectional area)*temp*density*layer_height)
-    #then multiply by the specific heat of water 
-    dz = 0.5 #thickness for each layer, hardcoded for now
+    #then multiply by the specific heat of water
+    dz = 0.5 #thickness for each layer
     cw = 4186 #specific heat of water
-#    energy = torch.empty_like(temps[0,:])
-    n_depths = 50
-#    depth_areas = depth_areas.view(n_depths,1).expand(n_depths, temps.size()[1])
-
-#    print(depth_areas)
     depth_areas = tf.reshape(depth_areas,[n_depths,1])
     energy = tf.reduce_sum(tf.multiply(tf.cast(depth_areas,tf.float32),temps)*densities*dz*cw,0)
     return energy
@@ -175,15 +117,9 @@ def calculate_lake_energy(temps, densities, depth_areas):
 
 def calculate_lake_energy_deltas(energies, combine_days, surface_area):
     #given a time series of energies, compute and return the differences
-    # between each time step, or time step interval (parameter @combine_days)
-    # as specified by parameter @combine_days
-#    energy_deltas = torch.empty_like(energies[0:-combine_days])
+    # between each time step
     time = 86400 #seconds per day
-    # surface_area = 39865825
     energy_deltas = (energies[1:] - energies[:-1])/time/surface_area
-#    energy_deltas = (energies[1:] - energies[:-1])/(time*surface_area)
-    # for t in range(1, energy_deltas.size()[0]):
-    #     energy_deltas[t-1] = (energies[t+combine_days] - energies[t])/(time*surface_area) #energy difference converted to W/m^2
     return energy_deltas
 
 
@@ -203,30 +139,27 @@ def calculate_air_density(air_temp, rh):
 
     #water vapor mixing ratio (from GLM code glm_surface.c)
     r = mwrw2a * vapPressure/(p - vapPressure)
-    # print( 0.348*(1+r)/(1+1.61*r)*(p/(air_temp+273.15)))
-    # print("vs")
-    # print(1.0/c_gas * (1 + r)/(1 + r/mwrw2a) * p/(air_temp + 273.15))
-    # sys.exit()
-    # return 0.348*(1+r)/(1+1.61*r)*(p/(air_temp+273.15))
-    return (1.0/c_gas * (1 + r)/(1 + r/mwrw2a) * p/(air_temp + 273.15))*100# 
+    return (1.0/c_gas * (1 + r)/(1 + r/mwrw2a) * p/(air_temp + 273.15))*100
+
+
 def calculate_heat_flux_sensible(surf_temp, air_temp, rel_hum, wind_speed):
     #equation 22 in GLM/GLEON paper(et al Hipsey)
     #GLM code ->  Q_sensibleheat = -CH * (rho_air * 1005.) * WindSp * (Lake[surfLayer].Temp - MetData.AirTemp);
-    #calculate air density 
+    #calculate air density
     rho_a = calculate_air_density(air_temp, rel_hum)
 
     #specific heat capacity of air in J/(kg*C)
     c_a = 1005.
-
 
     #bulk aerodynamic coefficient for sensible heat transfer
     c_H = 0.0013
 
     #wind speed at 10m
     U_10 = calculate_wind_speed_10m(wind_speed)
-    # U_10 = wind_speed
+
     return -rho_a*c_a*c_H*U_10*(surf_temp - air_temp)
- 
+
+
 def calculate_heat_flux_latent(surf_temp, air_temp, rel_hum, wind_speed):
     #equation 23 in GLM/GLEON paper(et al Hipsey)
     #GLM code-> Q_latentheat = -CE * rho_air * Latent_Heat_Evap * (0.622/p_atm) * WindSp * (SatVap_surface - MetData.SatVapDef)
@@ -242,9 +175,8 @@ def calculate_heat_flux_latent(surf_temp, air_temp, rel_hum, wind_speed):
     lambda_v = 2.453e6
 
     #wind speed at 10m height
-    # U_10 = wind_speed
     U_10 = calculate_wind_speed_10m(wind_speed)
-# 
+
     #ratio of molecular weight of water to that of dry air
     omega = 0.622
 
@@ -260,14 +192,12 @@ def calculate_vapour_pressure_air(rel_hum, temp):
     rh_scaling_factor = 1
     return rh_scaling_factor * (rel_hum / 100) * calculate_vapour_pressure_saturated(temp)
 
-def calculate_vapour_pressure_saturated(temp):
-    # returns in miilibars
-    # print(torch.pow(10, (9.28603523 - (2332.37885/(temp+273.15)))))
 
-    #Converted pow function to exp function workaround pytorch not having autograd implemented for pow
-#    exponent = (9.28603523 - (2332.37885/(temp+273.15))*np.log(10))
+def calculate_vapour_pressure_saturated(temp):
+    # returns in millibars
     exponent = (9.28603523 - (2332.37885/(temp+273.15)))*np.log(10)
     return tf.exp(exponent)
+
 
 def calculate_wind_speed_10m(ws, ref_height=2.):
     #from GLM code glm_surface.c
@@ -276,23 +206,16 @@ def calculate_wind_speed_10m(ws, ref_height=2.):
 
 
 def calculate_energy_fluxes(phys, surf_temps, combine_days):
-    # print("surface_depth = ", phys[0:5,1])
-#    fluxes = torch.empty_like(phys[:-combine_days-1,0])
-
-    # E = phys_operations.calculate_heat_flux_latent(t_s, air_temp, rel_hum, ws)
-    # H = phys_operations.calculate_heat_flux_sensible(t_s, air_temp, rel_hum, ws)
-#    time = 86400 #seconds per day
-#    surface_area = 39865825 
-    e_s = 0.985 #emissivity of water, given by Jordan
-    alpha_sw = 0.07 #shortwave albedo, given by Jordan Read
-    alpha_lw = 0.03 #longwave, albeda, given by Jordan Read
+    e_s = 0.985 #emissivity of water
+    alpha_sw = 0.07 #shortwave albedo
+    alpha_lw = 0.03 #longwave albedo
     sigma = 5.67e-8 #Stefan-Baltzmann constant
     R_sw_arr = phys[:-1,2] + (phys[1:,2]-phys[:-1,2])/2
     R_lw_arr = phys[:-1,3] + (phys[1:,3]-phys[:-1,3])/2
     R_lw_out_arr = e_s*sigma*(tf.pow(surf_temps[:]+273.15, 4))
     R_lw_out_arr = R_lw_out_arr[:-1] + (R_lw_out_arr[1:]-R_lw_out_arr[:-1])/2
 
-    air_temp = phys[:-1,4] 
+    air_temp = phys[:-1,4]
     air_temp2 = phys[1:,4]
     rel_hum = phys[:-1,5]
     rel_hum2 = phys[1:,5]
@@ -307,24 +230,18 @@ def calculate_energy_fluxes(phys, surf_temps, combine_days):
     E = (E + E2)/2
     H = (H + H2)/2
 
-#    #test
-#    print(R_sw_arr)
-#    print(R_lw_arr)
-#    print(R_lw_out_arr)
-#    print(E)
-#    print(H)
-#    fluxes = R_sw_arr[:-1]*(1-alpha_sw) + R_lw_arr[:-1]*(1-alpha_lw) - R_lw_out_arr[:-1] + E[:-1]
     fluxes = (R_sw_arr[:-1]*(1-alpha_sw) + R_lw_arr[:-1]*(1-alpha_lw) - R_lw_out_arr[:-1] + E[:-1] + H[:-1])
     return fluxes
 
 
-unsup_inputs = tf.placeholder("float", [None, n_steps, input_size]) #tf.float32
+''' Continue Graph Definition '''
+
+unsup_inputs = tf.placeholder("float", [None, n_steps, input_size])
 
 with tf.variable_scope("rnn", reuse=True) as scope_sp:
     state_series_xu, current_state_xu = tf.nn.dynamic_rnn(lstm_cell, unsup_inputs, dtype=tf.float32, scope=scope_sp) 
 
 pred_u=[]
-#pred_s = []
 for i in range(n_steps):
     tp2 = state_series_xu[:,i,:]
     pt2 = tf.matmul(tp2,w_fin)+b_fin
@@ -348,30 +265,28 @@ unsup_loss,a,b,c = calculate_ec_loss(unsup_inputs,
                                        combine_days=1)
 
 
-#plam = 0.15
+cost = r_cost + elam*unsup_loss
 
-cost = r_cost + elam*unsup_loss#+plam*plos+plam*plos_u
-
-#cost = tf.reduce_mean(tf.nn.(labels = y, logits = pred)) # + l2 # Softmax loss
-#optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost) # Adam Optimizer
 tvars = tf.trainable_variables()
 for i in tvars:
     print(i)
-saver = tf.train.Saver(max_to_keep=3)
 grads = tf.gradients(cost, tvars)
+
+saver = tf.train.Saver(max_to_keep=5)
+
 optimizer = tf.train.AdamOptimizer(learning_rate)
 train_op = optimizer.apply_gradients(zip(grads, tvars))
 
 
-# load data ---------------------------------------------------------------
+''' Load data '''
+
 x_full = np.load(os.path.join(args.data_path, 'processed_features.npy'))
 x_raw_full = np.load(os.path.join(args.data_path, 'features.npy'))
 diag_full = np.load(os.path.join(args.data_path, 'diag.npy'))
 
+# ['DOY', 'depth', 'ShortWave', 'LongWave', 'AirTemp', 'RelHum', 'WindSpeed', 'Daily.Qe', 'Daily.Qh', 'Has.Black.Ice']
 phy_full = np.concatenate((x_raw_full[:,:,:-2],diag_full),axis=2)
 
-
-# training and testing ------------------------------------------------------
 new_dates = np.load(os.path.join(args.data_path, 'dates.npy'), allow_pickle=True)
 
 train_data = feather.read_dataframe(os.path.join(args.data_path, 'labels_train.feather'))
@@ -441,12 +356,9 @@ for i in range(1,N_sec+1):
 
 x_f = x_train
 p_f = p_train
-# finish loading data --------------------------------------------------------
 
-#total_batch = int(x_train.shape[0]/batch_size)
-# merr = 20
-# metr = 20
-# ploss=10000
+
+''' Train '''
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
@@ -456,8 +368,6 @@ with tf.Session() as sess:
         saver.restore(sess, os.path.join(args.restore_path, 'pretrained_model.ckpt'))
         
     for epoch in range(epochs):
-#        
-#        for i in range(total_batch - 1): # better code?
         _, loss,rc,ec,aa,bb,cc,prd1 = sess.run(
                 [train_op, cost,r_cost,unsup_loss,a,b,c,pred],
                 feed_dict = {
@@ -474,8 +384,7 @@ with tf.Session() as sess:
               "{:.4f}".format(loss) + ", Rc= " + \
               "{:.4f}".format(rc) + ", Ec= " + \
               "{:.4f}".format(ec))
-        
-        
+
     loss_te,prd = sess.run([r_cost,pred], feed_dict = {x: x_train, y: y_test, m: m_test, bt_sz: n_depths*N_sec})
                 
     print("Loss_te " + "{:.4f}".format(loss_te) )
