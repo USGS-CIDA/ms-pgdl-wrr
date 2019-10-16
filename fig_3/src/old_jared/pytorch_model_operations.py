a5c2610edb16763b3e2863fd832f2c9e90d62ef2
Alison Appling <aappling@usgs.gov>
2019-10-16 13:01:17 -0400

    delete old fig3 files

--- fig_3/src/old_jared/pytorch_model_operations.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def saveModel(model_state, optimizer_state, save_path):
-    state = {'state_dict': model_state,
-                    'optimizer': optimizer_state }
-    torch.save(state, save_path)
